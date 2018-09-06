514c377b969273ec64b9032b75bacc798fe7ac12
Wangweizhen <hawking.rei@gmail.com>
2018-09-06 14:04:19 +0800

    feat: add zstd

---
+++ compress/zstd_reader_from_writer.go
@@ -0,0 +1,21 @@
+package compress
+
+import (
+	"io"
+
+	"github.com/DataDog/zstd"
+)
+
+type ZstdReaderFromWriter struct {
+	zstd.Writer
+}
+
+func NewZstdReaderFromWriter(dst io.Writer) *ZstdReaderFromWriter {
+	zstdWriter := zstd.NewWriterLevel(dst, 3)
+	return &ZstdReaderFromWriter{Writer: *zstdWriter}
+}
+
+func (writer *ZstdReaderFromWriter) ReadFrom(reader io.Reader) (n int64, err error) {
+	n, err = fastCopy(writer, reader)
+	return
+}
